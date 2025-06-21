4389c00cc46328dbc2a192e546c4860d67963379
MBrillian354 <brillian354@gmail.com>
2025-06-21 12:49:29 +0700

    Fix dashboard data

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const Branch = require('../models/branch');
-const TeachingGroup = require('../models/teachingGroup');
 const Class = require('../models/class')
-const TeachingGroupYear = require('../models/teachingGroupYear')
+const branchYear = require('../models/branchYear')
 const Student = require('../models/student')
 const Teacher = require('../models/teacher')
 const Attendance = require('../models/attendance');
+const SubBranch = require('../models/subBranch');
+
 
 
 const getDashboardData = async (req, res, next) => {
@@ -19,16 +20,16 @@ const getDashboardData = async (req, res, next) => {
 
         let dashboardData = {}
 
-        if (userRole === 'teachingGroupAdmin') {
+        if (userRole === 'subBranchAdmin') {
             const user = await User.findById(userId)
 
-            const teachingGroupYears = await TeachingGroupYear.find({ teachingGroupId: user.teachingGroupId }).select('_id');
-            const teachingGroupYearIds = teachingGroupYears.map(tgy => tgy._id);
+            const branchYears = await branchYear.find({ subBranchId: user.subBranchId }).select('_id');
+            const branchYearIds = branchYears.map(tgy => tgy._id);
 
-            const classCount = await Class.countDocuments({ teachingGroupYearId: { $in: teachingGroupYearIds } });
+            const classCount = await Class.countDocuments({ branchYearId: { $in: branchYearIds } });
 
             // Find all users (students) in this teaching group
-            const studentUsers = await User.find({ teachingGroupId: user.teachingGroupId }).select('_id');
+            const studentUsers = await User.find({ subBranchId: user.subBranchId }).select('_id');
             const studentUserIds = studentUsers.map(u => u._id);
 
             // Count students whose userId is in studentUserIds
@@ -52,7 +53,7 @@ const getDashboardData = async (req, res, next) => {
             const attendancePercentage = attendanceCount === 0 ? 0 : (attendancePresentCount / attendanceCount * 100);
 
             dashboardData = {
-                "Kelas": classCount,
+                // "Kelas": classCount,
                 "Peserta Didik": studentCount,
                 "Tenaga Pendidik": teacherCount,
                 "Kehadiran": attendancePercentage
@@ -63,7 +64,7 @@ const getDashboardData = async (req, res, next) => {
 
         if (userRole === 'admin' || userRole === 'curriculum') {
             const branchCount = await Branch.countDocuments()
-            const teachingGroupCount = await TeachingGroup.countDocuments()
+            const subBranchCount = await SubBranch.countDocuments()
             const classCount = await Class.countDocuments()
             const studentCount = await Student.countDocuments()
             const teacherCount = await Teacher.countDocuments()
@@ -76,7 +77,7 @@ const getDashboardData = async (req, res, next) => {
 
             dashboardData = {
                 "Desa": branchCount,
-                "Kelompok": teachingGroupCount,
+                "Kelompok": subBranchCount,
                 "Kelas": classCount,
                 "Peserta Didik": studentCount,
                 "Tenaga Pendidik": teacherCount,
