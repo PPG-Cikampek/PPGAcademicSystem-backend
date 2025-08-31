9a7a18f4976795a36219400a2ef32610a381e305
MBrillian354 <brillian354@gmail.com>
2025-08-31 17:57:31 +0700

    Fetching students now include isActive status

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -1,21 +1,48 @@
-const HttpError = require('../models/http-error')
-const mongoose = require('mongoose');
+const HttpError = require("../models/http-error");
+const mongoose = require("mongoose");
 
-const SubBranch = require('../models/subBranch');
-const Student = require('../models/student');
-const User = require('../models/user');
+const SubBranch = require("../models/subBranch");
+const Student = require("../models/student");
+const User = require("../models/user");
 
 const getStudents = async (req, res, next) => {
     let students;
     try {
         students = await Student.find()
-            .populate({ path: 'userId', select: 'subBranchId', populate: { path: 'subBranchId', select: 'name', populate: { path: 'branchId', select: 'name' } } })
+            .populate({
+                path: "userId",
+                select: "subBranchId",
+                populate: {
+                    path: "subBranchId",
+                    select: "name",
+                    populate: { path: "branchId", select: "name" },
+                },
+            })
+            .populate({
+                path: "classIds",
+                select: ["name", "subBranchId"],
+                populate: {
+                    path: "teachingGroupId",
+                    select: "branchYearId",
+                    populate: {
+                        path: "branchYearId",
+                        select: ["name", "isActive"],
+                    },
+                },
+            })
+            .sort({ nis: 1 });
 
+        students = students.map((student) => {
+            const isActive = student.classIds.some(
+                (classId) => classId.teachingGroupId.branchYearId.isActive
+            );
+            return { ...student.toObject({ getters: true }), isActive };
+        });
     } catch (err) {
-        console.log(err)
-        return next(new HttpError("Internal server error occured!", 500))
+        console.log(err);
+        return next(new HttpError("Internal server error occured!", 500));
     }
-    console.log('Get all students requested')
+    console.log("Get all students requested");
     res.json({ students });
 };
 
@@ -26,33 +53,49 @@ const getStudentById = async (req, res, next) => {
     let student;
 
     try {
-        if (populate === 'attendance') {
+        if (populate === "attendance") {
             student = await Student.findById(studentId)
-                .populate({ path: 'userId', select: 'subBranchId', populate: { path: 'subBranchId', select: 'name', populate: { path: 'branchId', select: 'name' } } })
                 .populate({
-                    path: 'attendanceIds',
-                    populate: { path: 'classId', select: 'name' }
+                    path: "userId",
+                    select: "subBranchId",
+                    populate: {
+                        path: "subBranchId",
+                        select: "name",
+                        populate: { path: "branchId", select: "name" },
+                    },
                 })
+                .populate({
+                    path: "attendanceIds",
+                    populate: { path: "classId", select: "name" },
+                });
         } else {
             student = await Student.findById(studentId)
-                .populate({ path: 'userId', select: 'subBranchId', populate: { path: 'subBranchId', select: 'name', populate: { path: 'branchId', select: 'name' } } })
                 .populate({
-                    path: 'classIds',
-                    populate: { path: 'teachingGroupId' }
+                    path: "userId",
+                    select: "subBranchId",
+                    populate: {
+                        path: "subBranchId",
+                        select: "name",
+                        populate: { path: "branchId", select: "name" },
+                    },
                 })
+                .populate({
+                    path: "classIds",
+                    populate: { path: "teachingGroupId" },
+                });
         }
     } catch (err) {
         console.error(err);
         return next(new HttpError("Internal server error occurred!", 500));
     }
 
-    console.log('Get student requested');
+    console.log("Get student requested");
     if (!student) {
         return next(new HttpError("Student not found!", 404));
     }
 
     res.json({ student: student.toObject({ getters: true }) });
-}
+};
 
 const getStudentByNis = async (req, res, next) => {
     const nis = req.params.nis;
@@ -70,16 +113,15 @@ const getStudentByNis = async (req, res, next) => {
             nis: student[0].nis,
             name: student[0].name,
             email: student[0].email,
-        } // Access the first element
-
+        }; // Access the first element
     } catch (err) {
-        console.log(err)
-        return next(new HttpError("Internal server error occured!", 500))
+        console.log(err);
+        return next(new HttpError("Internal server error occured!", 500));
     }
 
-    console.log(`Get student with ${nis}`)
+    console.log(`Get student with ${nis}`);
     res.json({ student });
-}
+};
 
 const getStudentByUserId = async (req, res, next) => {
     const userId = req.params.userId;
@@ -87,8 +129,15 @@ const getStudentByUserId = async (req, res, next) => {
     let student;
 
     try {
-        student = await Student.findOne({ userId })
-            .populate({ path: 'userId', select: 'subBranchId', populate: { path: 'subBranchId', select: 'name', populate: { path: 'branchId', select: 'name' } } })
+        student = await Student.findOne({ userId }).populate({
+            path: "userId",
+            select: "subBranchId",
+            populate: {
+                path: "subBranchId",
+                select: "name",
+                populate: { path: "branchId", select: "name" },
+            },
+        });
         // .populate({
         //     path: 'classIds',
         //     populate: [
@@ -103,7 +152,7 @@ const getStudentByUserId = async (req, res, next) => {
 
     console.log(`Get student by userId ${userId} requested`);
     res.json({ student: student.toObject({ getters: true }) });
-}
+};
 
 const getStudentReportByIdAndClassId = async (req, res, next) => {
     const { studentId, classId } = req.body;
@@ -111,20 +160,27 @@ const getStudentReportByIdAndClassId = async (req, res, next) => {
     let student;
     try {
         student = await Student.find({ _id: studentId, classIds: classId })
-            .populate({ path: 'userId', select: 'subBranchId', populate: { path: 'subBranchId', select: 'name', populate: { path: 'branchId', select: 'name' } } })
             .populate({
-                path: 'attendanceIds',
-                populate: { path: 'classId', select: 'name' }
+                path: "userId",
+                select: "subBranchId",
+                populate: {
+                    path: "subBranchId",
+                    select: "name",
+                    populate: { path: "branchId", select: "name" },
+                },
             })
-
+            .populate({
+                path: "attendanceIds",
+                populate: { path: "classId", select: "name" },
+            });
     } catch (err) {
         console.error(err);
         return next(new HttpError("Internal server error occurred!", 500));
     }
 
-    console.log('Get student requested');
+    console.log("Get student requested");
     res.json({ student: student.toObject({ getters: true }) });
-}
+};
 
 const getStudentsByBranchId = async (req, res, next) => {
     const branchId = req.params.branchId;
@@ -133,38 +189,55 @@ const getStudentsByBranchId = async (req, res, next) => {
     try {
         // 1. Find subBranches with the given branchId
         const subBranches = await SubBranch.find({ branchId: branchId });
-        const subBranchIds = subBranches.map(sb => sb._id);
+        const subBranchIds = subBranches.map((sb) => sb._id);
 
         // 2. Find users with subBranchId in subBranchIds
         const users = await User.find({ subBranchId: { $in: subBranchIds } });
 
         // 3. Extract user IDs
-        const userIds = users.map(user => user._id);
+        const userIds = users.map((user) => user._id);
 
         // 4. Find students based on user IDs
         students = await Student.find({ userId: { $in: userIds } })
             .populate({
-                path: 'userId',
-                select: 'subBranchId',
+                path: "userId",
+                select: "subBranchId",
                 populate: {
-                    path: 'subBranchId',
-                    select: 'name',
+                    path: "subBranchId",
+                    select: "name",
                     populate: {
-                        path: 'branchId',
-                        select: 'name'
-                    }
-                }
+                        path: "branchId",
+                        select: "name",
+                    },
+                },
             })
-            .sort({ nis: 1 })
+            .populate({
+                path: "classIds",
+                select: ["name", "subBranchId"],
+                populate: {
+                    path: "teachingGroupId",
+                    select: "branchYearId",
+                    populate: {
+                        path: "branchYearId",
+                        select: ["name", "isActive"],
+                    },
+                },
+            })
+            .sort({ nis: 1 });
 
+        students = students.map((student) => {
+            const isActive = student.classIds.some(
+                (classId) => classId.teachingGroupId.branchYearId.isActive
+            );
+            return { ...student.toObject({ getters: true }), isActive };
+        });
     } catch (err) {
-        console.log(err)
-        return next(new HttpError("Internal server error occured!", 500))
+        console.log(err);
+        return next(new HttpError("Internal server error occured!", 500));
     }
-    console.log('Get students by branch ID requested')
-    res.json({ students })
+    console.log("Get students by branch ID requested");
+    res.json({ students });
 };
-
 
 const getStudentsBySubBranchId = async (req, res, next) => {
     const subBranchId = req.params.subBranchId;
@@ -175,45 +248,48 @@ const getStudentsBySubBranchId = async (req, res, next) => {
         const users = await User.find({ subBranchId: subBranchId });
 
         // 2. Extract user IDs
-        const userIds = users.map(user => user._id);
+        const userIds = users.map((user) => user._id);
 
         // 3. Find students based on user IDs
         students = await Student.find({ userId: { $in: userIds } })
             .populate({
-                path: 'userId',
-                select: 'subBranchId',
+                path: "userId",
+                select: "subBranchId",
                 populate: {
-                    path: 'subBranchId',
-                    select: 'name',
+                    path: "subBranchId",
+                    select: "name",
                     populate: {
-                        path: 'branchId',
-                        select: 'name'
-                    }
-                }
+                        path: "branchId",
+                        select: "name",
+                    },
+                },
             })
             .populate({
-                path: 'classIds',
-                select: ['name', 'subBranchId'],
+                path: "classIds",
+                select: ["name", "subBranchId"],
                 populate: {
-                    path: 'teachingGroupId',
-                    select: 'branchYearId',
-                    populate: { path: 'branchYearId', select: ['name', 'isActive'] }
-                }
+                    path: "teachingGroupId",
+                    select: "branchYearId",
+                    populate: {
+                        path: "branchYearId",
+                        select: ["name", "isActive"],
+                    },
+                },
             })
             .sort({ nis: 1 });
 
-
-        students = students.map(student => {
-            const isActive = student.classIds.some(classId => classId.teachingGroupId.branchYearId.isActive);
+        students = students.map((student) => {
+            const isActive = student.classIds.some(
+                (classId) => classId.teachingGroupId.branchYearId.isActive
+            );
             return { ...student.toObject({ getters: true }), isActive };
         });
-
     } catch (err) {
-        console.log(err)
-        return next(new HttpError("Internal server error occured!", 500))
+        console.log(err);
+        return next(new HttpError("Internal server error occured!", 500));
     }
-    console.log('Get users requested')
-    res.json({ students })
+    console.log("Get users requested");
+    res.json({ students });
 };
 
 const createStudent = async (req, res, next) => {
@@ -225,11 +301,11 @@ const createStudent = async (req, res, next) => {
     } catch (err) {
         console.log(err);
         console.log(identifiedUser);
-        return next(new HttpError('Internal server error!', 500));
+        return next(new HttpError("Internal server error!", 500));
     }
 
     if (!identifiedUser) {
-        return next(new HttpError('User tidak ditemukan!', 500));
+        return next(new HttpError("User tidak ditemukan!", 500));
     }
 
     const createdStudent = new Student({
@@ -244,73 +320,101 @@ const createStudent = async (req, res, next) => {
         image: "",
         thumbnail: "",
         attendanceIds: [],
-        classIds: []
+        classIds: [],
     });
 
     try {
         await createdStudent.save();
     } catch (err) {
         console.log(err);
-        const error = new HttpError('Gagal menambahkan peserta didik!', 500);
+        const error = new HttpError("Gagal menambahkan peserta didik!", 500);
         return next(error);
     }
 
-    res.status(202).json({ message: `Berhasil mendaftarkan peserta didik! ${name}!`, student: createdStudent });
-}
+    res.status(202).json({
+        message: `Berhasil mendaftarkan peserta didik! ${name}!`,
+        student: createdStudent,
+    });
+};
 
 const updateStudent = async (req, res, next) => {
-    const { nis, name, dateOfBirth, gender, parentName, parentPhone, address, thumbnail } = req.body;
+    const {
+        nis,
+        name,
+        dateOfBirth,
+        gender,
+        parentName,
+        parentPhone,
+        address,
+        thumbnail,
+    } = req.body;
     const studentId = req.params.studentId;
 
     let student;
     try {
-        const updateData = { ...(nis && { nis }), name, dateOfBirth, gender, address, parentName, parentPhone, isProfileComplete: true };
+        const updateData = {
+            ...(nis && { nis }),
+            name,
+            dateOfBirth,
+            gender,
+            address,
+            parentName,
+            parentPhone,
+            isProfileComplete: true,
+        };
         if (req.file) {
-            updateData.image = req.file.path.replace(/\\/g, '/');
+            updateData.image = req.file.path.replace(/\\/g, "/");
             updateData.originalImagePath = req.file.path;
         }
         if (thumbnail) {
             updateData.thumbnail = thumbnail;
         }
 
-        student = await Student.findByIdAndUpdate(
-            studentId,
-            updateData,
-            { new: true, runValidators: true }
-        );
+        student = await Student.findByIdAndUpdate(studentId, updateData, {
+            new: true,
+            runValidators: true,
+        });
 
         if (student) {
             const userUpdate = { name: student.name };
             if (req?.file?.path) userUpdate.image = req.file.path;
             if (thumbnail) userUpdate.thumbnail = thumbnail;
-            await User.findByIdAndUpdate(
-                student.userId,
-                userUpdate,
-                { new: true, runValidators: true }
-            );
+            await User.findByIdAndUpdate(student.userId, userUpdate, {
+                new: true,
+                runValidators: true,
+            });
         }
-
     } catch (err) {
         console.error(err);
-        const error = new HttpError('Something went wrong while updating the student.', 500);
+        const error = new HttpError(
+            "Something went wrong while updating the student.",
+            500
+        );
         return next(error);
     }
 
     if (!student) {
-        return next(new HttpError(`Could not find a student with ID '${studentId}'`, 404));
+        return next(
+            new HttpError(
+                `Could not find a student with ID '${studentId}'`,
+                404
+            )
+        );
     }
 
-    console.log(`Student with nis '${student.nis}' updated!`)
-    res.status(200).json({ message: 'Berhasil melengkapi profile siswa!', student: student.toObject({ getters: true }) })
+    console.log(`Student with nis '${student.nis}' updated!`);
+    res.status(200).json({
+        message: "Berhasil melengkapi profile siswa!",
+        student: student.toObject({ getters: true }),
+    });
+};
 
-}
-
-exports.getStudentById = getStudentById
-exports.getStudentByNis = getStudentByNis
-exports.getStudentByUserId = getStudentByUserId
-exports.getStudentReportByIdAndClassId = getStudentReportByIdAndClassId
-exports.getStudents = getStudents
-exports.getStudentsByBranchId = getStudentsByBranchId
-exports.getStudentsBySubBranchId = getStudentsBySubBranchId
-exports.createStudent = createStudent
-exports.updateStudent = updateStudent
+exports.getStudentById = getStudentById;
+exports.getStudentByNis = getStudentByNis;
+exports.getStudentByUserId = getStudentByUserId;
+exports.getStudentReportByIdAndClassId = getStudentReportByIdAndClassId;
+exports.getStudents = getStudents;
+exports.getStudentsByBranchId = getStudentsByBranchId;
+exports.getStudentsBySubBranchId = getStudentsBySubBranchId;
+exports.createStudent = createStudent;
+exports.updateStudent = updateStudent;
