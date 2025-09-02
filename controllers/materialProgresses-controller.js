03f0c321f454de99d1cb3b397f29358cc1a5c292
MBrillian354 <brillian354@gmail.com>
2025-09-02 22:31:58 +0700

    Fix date range calculation for getProgressesByUserId

--- controllers/materialProgresses-controller.js
+++ controllers/materialProgresses-controller.js
@@ -1,51 +1,54 @@
-const HttpError = require('../models/http-error')
-const mongoose = require('mongoose');
+const HttpError = require("../models/http-error");
+const mongoose = require("mongoose");
 
-const User = require('../models/user');
-const Class = require('../models/class');
-const MaterialProgress = require('../models/materialProgress')
+const User = require("../models/user");
+const Class = require("../models/class");
+const MaterialProgress = require("../models/materialProgress");
 
 const getProgressesByUserId = async (req, res, next) => {
     const { week } = req.query;
-    const { userId } = req.params
+    const { userId } = req.params;
 
-    console.log(week)
-    console.log(userId)
+    console.log(week);
+    console.log(userId);
 
     let progresses;
 
     try {
         const startDate = new Date(week);
         const endDate = new Date(startDate);
-        endDate.setDate(startDate.getDate() + 6);
         startDate.setDate(startDate.getDate() - 1);
+        endDate.setDate(endDate.getDate() + 6);
+
+        console.log(startDate, endDate);
 
         progresses = await MaterialProgress.find({
             userId,
-            // forDate: { $gte: startDate, $lte: endDate }
+            forDate: { $gte: startDate, $lte: endDate },
         }).sort({ forDate: -1 });
     } catch (err) {
         console.error(err);
         return next(new HttpError("Internal server error occurred!", 500));
     }
-    console.log(progresses)
-    console.log('Get progresses requested');
-    res.json({ progresses: progresses.map(x => x.toObject({ getters: true })) });
+    console.log("Get progresses requested");
+    res.json({
+        progresses: progresses.map((x) => x.toObject({ getters: true })),
+    });
 };
 
 const createProgress = async (req, res, next) => {
-    const { category, material, classId, userId } = req.body
+    const { category, material, classId, userId } = req.body;
 
     let identifiedClass;
     try {
         identifiedClass = await Class.findById(classId);
     } catch (err) {
         console.log(err);
-        return next(new HttpError('Internal server error!', 500));
+        return next(new HttpError("Internal server error!", 500));
     }
 
     if (!identifiedClass) {
-        return next(new HttpError('Class not found!', 500));
+        return next(new HttpError("Class not found!", 500));
     }
 
     let identifiedUser;
@@ -53,13 +56,12 @@ const createProgress = async (req, res, next) => {
         identifiedUser = await User.findById(userId);
     } catch (err) {
         console.log(err);
-        return next(new HttpError('Internal server error!', 500));
+        return next(new HttpError("Internal server error!", 500));
     }
 
     if (!identifiedUser) {
-        return next(new HttpError('user not found!', 500));
+        return next(new HttpError("user not found!", 500));
     }
-
 
     const forDate = new Date().toISOString();
     const timestamp = new Date().toISOString();
@@ -70,19 +72,22 @@ const createProgress = async (req, res, next) => {
         category,
         material,
         userId,
-        classId
-    })
+        classId,
+    });
 
     try {
-        await createdProgress.save()
+        await createdProgress.save();
     } catch (err) {
         console.log(err);
-        const error = new HttpError('Gagal menyimpan pencapaian materi!', 500);
+        const error = new HttpError("Gagal menyimpan pencapaian materi!", 500);
         return next(error);
     }
 
-    res.json({ message: `Berhasil menyimpan pencapaian materi!`, journal: createdProgress.toObject({ getters: true }) });
-}
+    res.json({
+        message: `Berhasil menyimpan pencapaian materi!`,
+        journal: createdProgress.toObject({ getters: true }),
+    });
+};
 
 exports.getProgressesByUserId = getProgressesByUserId;
-exports.createProgress = createProgress
+exports.createProgress = createProgress;
