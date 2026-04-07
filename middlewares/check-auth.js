dffa309da5e5dbc3a46bd924c2801d18b3247a58
Muhammad Brillian <brillian354@gmail.com>
2026-04-07 21:35:57 +0700

    Fully implement SSO

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -1,7 +1,12 @@
-const { jwtVerify } = require("jose");
+const { jwtVerify, createRemoteJWKSet } = require("jose");
 const HttpError = require("../models/http-error");
 
-const iamSecret = new TextEncoder().encode(process.env.IAM_JWT_SECRET);
+const IAM_BASE_URL = process.env.IAM_BASE_URL || "http://localhost:3001";
+const IAM_JWKS_URL = process.env.IAM_JWKS_URL || `${IAM_BASE_URL.replace(/\/+$/, "")}/oauth/jwks`;
+const IAM_ISSUER = process.env.IAM_ISSUER || IAM_BASE_URL;
+const IAM_AUDIENCE = process.env.IAM_AUDIENCE || "ppg-cikampek-apps";
+
+const JWKS = createRemoteJWKSet(new URL(IAM_JWKS_URL));
 
 module.exports = async (req, res, next) => {
     try {
@@ -15,9 +20,9 @@ module.exports = async (req, res, next) => {
         const token = authHeader.split(" ")[1];
         if (!token) throw new Error("No token");
 
-        const { payload } = await jwtVerify(token, iamSecret, {
-            issuer: "iam.ppgcikampek.id",
-            audience: "ppg-cikampek-apps",
+        const { payload } = await jwtVerify(token, JWKS, {
+            issuer: IAM_ISSUER,
+            audience: IAM_AUDIENCE,
         });
 
         req.userData = {
